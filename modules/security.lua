2949ec2b3982a597767bbc6ce12017948838f550
Eyoko <82108728+Eyoko1@users.noreply.github.com>
2026-03-31 22:11:30 +0100

    Test

--- modules/security.lua
+++ modules/security.lua
@@ -98,5 +98,5 @@ lje.vm.set_engine_call_hook(function(func, nargs, nresults, ...)
     callback = callpre("ljeutil/unknownenginecall", func, nargs, nresults, ...) or
                callpost("ljeutil/unknownenginecall", func, nargs, nresults, ...)
     recognisedcalls[func] = callback or detours.generic
-    goto do_hook
+    --goto do_hook
 end)
